3735fc75d6047f0f57c325164169d49e754d059b
pka5667 <63954083+pka5667@users.noreply.github.com>
2020-12-11 09:04:00 +0530

    Update Syllabus.js

--- js/Syllabus.js
+++ js/Syllabus.js
@@ -1,7 +1,8 @@
 var Syllabus = [
     ["All Branch Syllabus","10-12-2020","https://drive.google.com/file/d/1QmgDJRkMgjfJLuTHEBtpWH1NAxsdr61Z/view?usp=sharing"],
-    ["HS1401 Syllabus", "10-12-2020", "https://drive.google.com/file/d/1dqPqMjFzm8JEXU4qQkwWoDF-sUDFYTsd/view?usp=sharing"],
+    ["CH1401 Syllabus","11-12-2020","https://drive.google.com/file/d/1MFO1l5Kw924nOW74URU19M8Tjw1ht6EF/view?usp=sharing"],
     ["EC1401 Syllabus","10-12-2020","https://drive.google.com/file/d/1Jcjvpx1BfU36Bz39lIjdEUoOsDwuF_j5/view?usp=sharing"],
+    ["HS1401 Syllabus", "10-12-2020", "https://drive.google.com/file/d/1dqPqMjFzm8JEXU4qQkwWoDF-sUDFYTsd/view?usp=sharing"],
     ["MA1401 Syllabus","10-12-2020","https://drive.google.com/file/d/1E4D8DuBf91Xh40qG1jDy87xa7adt0xlq/view?usp=sharing"],
 ]
 
