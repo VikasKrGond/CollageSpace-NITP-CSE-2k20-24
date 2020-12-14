a2249abe46b9aaa5be559d9d168c827287c1f1df
pka5667 <63954083+pka5667@users.noreply.github.com>
2020-12-14 15:50:48 +0530

    Update Books.js

--- js/Books.js
+++ js/Books.js
@@ -3,6 +3,7 @@ var Books = [
     ["Engineering Chemistry BY SHASHI CHAWLA","11-12-2020","https://drive.google.com/file/d/1lFNaYGKMjpXyhrKK2RRLttL3HXaUkBnh/view?usp=sharing"],
     ["ENGINEERING DRAWING BY N.D BHATT", "11-12-2020", "https://drive.google.com/file/d/16J1FLpt8I2u--whz491080qA4dmJf3cq/view?usp=sharing"],
     ["Enginnering mathematics By B.S. Gerwal", "11-12-2020", "https://drive.google.com/file/d/1Uim1RRvaAoD9Y8RBzWtatHM25JT6zw52/view?usp=sharing"],
+    ["Professional Ethics and Human Values by R.S NAAGARAZAN","14-12-2020","https://drive.google.com/file/d/1H4y1ovcp_ZFL-a70IPLqGACwjFK-kMe3/view?usp=sharing"],
     ["Engineering ETHICS","11-12-2020","https://drive.google.com/file/d/1bCmj_O1W_GLaG_g5PpM79h--drIk7U9h/view?usp=sharing"],
     ["Programming In Ansi C_E. Balagurusamy","11-12-2020","https://drive.google.com/file/d/1jrodW-QTsPiwFkmMm8TVA3rnudVemttf/view?usp=sharing"]
 ]
